0c38f05979f62e6e6499990a378e68d95059f960
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-01 16:34:27 -0700

    added update method

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -32,4 +32,24 @@ class CreaturesController < ApplicationController
     #render creature to page
     render :show
   end
+  def edit
+    #get creature by id, store in creature_id var
+    creature_id = params[:id]
+    #creating instance var in which the found creature is stored
+    @creature = Creature.find_by_id(creature_id)
+    #render creature to page
+    render :edit
+  end
+  def update
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
+  end
+
 end
