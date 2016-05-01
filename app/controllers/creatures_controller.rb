59ee284515e5d0546117dda832166fe703f31fc0
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-01 00:40:39 -0700

    added method to update a creature and redirect to show page for the updated creature.

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -34,6 +34,33 @@ class CreaturesController < ApplicationController
 
     #render the show view (it has access to instance variable)
     render :show
-
   end
+
+  def edit
+    #get the creature id from the url params
+    creature_id = params[:id]
+    #use "creature_id" to find the creature in the db
+    #and save it to an instance var
+    @creature = Creature.find_by_id(creature_id)
+    render :edit
+  end
+
+  def update
+    #get creature id from url params
+    creature_id = params[:id]
+
+    #use id var to find creature in db and save to instance var
+    creature = Creature.find_by_id(creature_id)
+
+    #whitelist params and save them to var
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    #update creature
+    creature.update_attributes(creature_params)
+
+    #redirecto to show page
+    redirect_to creature_path(creature)
+    #redirect to creature_path (creature) is same as redirect_to "/creatures/#{creature.id}"
+  end
+
 end
