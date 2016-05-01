b3f03260e48b3bfd2a777be3b4226d1f07bd8c25
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-01 00:07:08 -0700

    added methods for each create and new.

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -5,4 +5,24 @@ class CreaturesController < ApplicationController
     #render the index view (it has access to instance var)
     render :index
   end
+
+  def new
+    @creature = Creature.new
+
+    render :new
+  end
+
+  def create
+    #whitelist params and save them to variable
+    creature_params = params.require(:creature).permit(:name, :description)
+    #create a new creature from creature_params
+    creature = Creature.new(creature_params)
+
+    #if creatures saves, redirecte to route that displays all creatures
+    if creature.saves
+      redirect_to creatures_path
+      # redirect_to creatures_path is equivalent to redirect_to "/creatures"
+    end
+  end
+
 end
