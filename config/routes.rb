cb97fac7a3e79c38fedce970493fe56edc399f56
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-01 16:41:19 -0700

    added delete route

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ post "/creatures", to: "creatures#create"
 get "creatures/:id", to: "creatures#show", as: "creature"
 get "creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
 patch "creatures/:id", to: "creatures#update"
-
+delete "creatures/:id", to: "creatures#delete"
 end
