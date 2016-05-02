d26e69584854efbe27b332fb8303482bb83e7b17
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-01 23:16:56 -0700

    saving changes

--- bog_app3/app/controllers/creatures_controller.rb
+++ bog_app3/app/controllers/creatures_controller.rb
@@ -18,10 +18,47 @@ class CreaturesController < ApplicationController
     creature = Creature.new(creature_params)
 
     # if creature saves, redirect to route that displays all creatures
-    if creature.saves
-      redirect_to creatures_path
+    if creature.save
+      redirect_to creature_path(creature)
       # redirect_to creatures_path is equivalent to:
       # redirect_to "/creatures"
     end
+  end
+  def show
+    #get creature by id, store in creature_id var
+    creature_id = params[:id]
+    #creating instance var in which store the found creature is stored
+    @creature = Creature.find_by_id(creature_id)
+    #render creature to page
+    render :show
+  end
+  def edit
+    #get creature by id, store in creature_id var
+    creature_id = params[:id]
+    #creating instance var in which the found creature is stored
+    @creature = Creature.find_by_id(creature_id)
+    #render creature to page
+    render :edit
+  end
+  def update
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
+  end
+
+  def delete
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id (creature_id)
+
+    creature.destroy
+
+    redirect_to creatures_path
   end
 end
--- bog_app3/config/routes.rb
+++ bog_app3/config/routes.rb
@@ -4,4 +4,8 @@ root "creatures#index"
 get "/creatures", to: "creatures#index", as: "creatures"
 get "/creatures/new", to: "creatures#new", as: "new_creatures"
 post "/creatures", to: "creatures#create"
+get "creatures/:id", to: "creatures#show", as: "creature"
+get "creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+patch "creatures/:id", to: "creatures#update"
+delete "creatures/:id", to: "creatures#delete"
 end
