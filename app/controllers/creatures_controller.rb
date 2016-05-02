a43a8377940625e77cc7ab4d5bddac726bc1629a
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-01 22:36:02 -0700

    added method to render form to create new creature on 'new' page

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -5,4 +5,19 @@ class CreaturesController < ApplicationController
     #render to index page
     render :index
   end
+  def new
+    render :new
+  end
+  def create
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    #create new creature from creature_params
+    creature = Creature.new(creature_params)
+
+    #if creature saves, redirect to route that displays all creatures
+    if creature.save
+      redirect_to creatures_path
+    end
+  end
 end
