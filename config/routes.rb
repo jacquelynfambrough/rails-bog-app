8f8ac39967de2d7507d0160ee3bbd4d94b3e4d6a
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-05-01 22:53:39 -0700

    added show route exit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get "/creatures", to: "creatures#index",  as: "creatures"
   get "/creatures/new", to: "creatures#new", as: "new_creatures"
-  post "/creatures", to: "creatures#create" 
+  post "/creatures", to: "creatures#create"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
 end
